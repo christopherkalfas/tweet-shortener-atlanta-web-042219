73a9fea84d2f4358882b06e52183715f6221ba6d
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-22 23:25:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,5 +40,9 @@ def selective_tweet_shortener(tweets)
 end 
 
 def shortened_tweet_truncator(tweet)
-  
+  if tweet.length > 140
+    word_substituter(tweet)[1..140] + "..."
+  else
+    tweet
+  end
 end 
