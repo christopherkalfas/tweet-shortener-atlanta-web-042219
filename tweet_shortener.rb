28fbb437b97d0b05c9413fc42970ef76ef2c8450
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-22 22:07:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,13 @@
 def dictionary = {
   "hi"=>"hello",
-  "2" => "to", "two", "too"
-  "4" => "for", "four"
-  "b" => "be"
-  "u" => "you"
-}
+  "2" => ["to", "two", "too"],
+  "4" => ["for", "four"],
+  "b" => "be",
+  "u" => "you",
+  "@" => "at",
+  "&" => "and"
+  }
+end
   
 
 
